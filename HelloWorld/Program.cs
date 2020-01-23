d8272864557125f88b5e97e6252326aad8092009
Brennan Harrison <brennan16@usf.edu>
2020-01-23 17:14:10 -0500

    Commit 1.0

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display "Hello World!"
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+
+        }
+    }
+}
